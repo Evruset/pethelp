f09eddbce9ac105a15b6207d41634737284264ab
Evruset <46761863+Evruset@users.noreply.github.com>
2026-06-25 00:12:39 +0300

    feat(telemed): register owner telemed APIs

--- backend/src/modules/telemed/telemed.module.ts
+++ backend/src/modules/telemed/telemed.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../../auth/auth.module';
 import { LiveKitWebhookController } from './livekit-webhook.controller';
 import { LiveKitWebhookService } from './livekit-webhook.service';
 import { LiveKitService } from './livekit.service';
+import { TelemedOwnerController } from './telemed-owner.controller';
+import { TelemedOwnerEndService } from './telemed-owner-end.service';
+import { TelemedOwnerService } from './telemed-owner.service';
+import { TelemedRoomCloseWorker } from './telemed-room-close.worker';
 import { TelemedService } from './telemed.service';
 import { TelemedSessionStartWorker } from './telemed-session-start.worker';
 import { TelemedSlaWorker } from './telemed-sla.worker';
 
 @Module({
-  controllers: [LiveKitWebhookController],
+  imports: [AuthModule],
+  controllers: [LiveKitWebhookController, TelemedOwnerController],
   providers: [
     LiveKitService,
     LiveKitWebhookService,
     TelemedService,
+    TelemedOwnerService,
+    TelemedOwnerEndService,
     TelemedSessionStartWorker,
     TelemedSlaWorker,
+    TelemedRoomCloseWorker,
   ],
-  exports: [TelemedService, LiveKitService],
+  exports: [TelemedService, LiveKitService, TelemedOwnerService],
 })
 export class TelemedModule {}
