3dad4778f4affe5bc61d463c63775615246ec040
Evruset <46761863+Evruset@users.noreply.github.com>
2026-06-25 22:10:22 +0300

    feat(owner): register appointment list service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { OwnerAuthController, OwnerProfileController } from './owner-auth.controller';
+import { OwnerAppointmentsService } from './owner-appointments.service';
 import { OwnerAuthService } from './owner-auth.service';
 import { OwnerPetController } from './owner-pet.controller';
 import { OwnerPetService } from './owner-pet.service';
@@ -12,7 +13,22 @@ import { WorkerAuthGuard } from './worker-auth.guard';
 @Module({
   imports: [JwtModule.register({})],
   controllers: [OwnerAuthController, OwnerProfileController, OwnerPetController],
-  providers: [JwtAuthGuard, RolesGuard, WorkerAuthGuard, OwnerAuthService, OwnerPetService],
-  exports: [JwtModule, JwtAuthGuard, RolesGuard, WorkerAuthGuard, OwnerAuthService, OwnerPetService],
+  providers: [
+    JwtAuthGuard,
+    RolesGuard,
+    WorkerAuthGuard,
+    OwnerAuthService,
+    OwnerAppointmentsService,
+    OwnerPetService,
+  ],
+  exports: [
+    JwtModule,
+    JwtAuthGuard,
+    RolesGuard,
+    WorkerAuthGuard,
+    OwnerAuthService,
+    OwnerAppointmentsService,
+    OwnerPetService,
+  ],
 })
 export class AuthModule {}
