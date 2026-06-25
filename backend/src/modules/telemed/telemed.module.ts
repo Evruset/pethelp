8797cab4a309b4113e37b2c165eadadeb9c93d25
Evruset <46761863+Evruset@users.noreply.github.com>
2026-06-25 09:12:05 +0300

    feat(telemed): register owner waiting room endpoint

--- backend/src/modules/telemed/telemed.module.ts
+++ backend/src/modules/telemed/telemed.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { LiveKitWebhookController } from './livekit-webhook.controller';
 import { LiveKitWebhookService } from './livekit-webhook.service';
 import { LiveKitService } from './livekit.service';
+import { TelemedOwnerSessionController } from './telemed-owner-session.controller';
+import { TelemedOwnerSessionService } from './telemed-owner-session.service';
 import { TelemedService } from './telemed.service';
 import { TelemedSessionStartWorker } from './telemed-session-start.worker';
 import { TelemedSlaWorker } from './telemed-sla.worker';
 
 @Module({
-  controllers: [LiveKitWebhookController],
+  controllers: [LiveKitWebhookController, TelemedOwnerSessionController],
   providers: [
     LiveKitService,
     LiveKitWebhookService,
     TelemedService,
+    TelemedOwnerSessionService,
     TelemedSessionStartWorker,
     TelemedSlaWorker,
   ],
-  exports: [TelemedService, LiveKitService],
+  exports: [TelemedService, TelemedOwnerSessionService, LiveKitService],
 })
 export class TelemedModule {}
